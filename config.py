94c0f7e669498b6af95ff7b2ad15f947a7d91a39
wangms <wangms@create80.com>
2019-05-31 01:02:43 +0800

    修改配置

--- config.py
+++ config.py
@@ -16,12 +16,12 @@ class BaseConfig(object):
     CATALOG_JSON = ''
     SECRET_KEY = '123456789'
     SEND_FILE_MAX_AGE_DEFAULT = timedelta(seconds=1)
-    NOTES_DIRCTORY = "D:\\MyNote"
+    NOTES_DIRCTORY = "E:\\MyNote"
     IMAGE_FOLDER = ".img"
     ITEM_FILE_SUFFIX = ".md"
     FOLDER_CONTENT_FILE = "init.md"
     SQLALCHEMY_DATABASE_URI = 'sqlite:///{}\\ideanote.db?check_same_thread=False'.format(NOTES_DIRCTORY)
-    REMOTE_URL = "https://git.coding.net/wmsgood/new_mynote.git"
+    REMOTE_URL = "git@git.coding.net:wmsgood/MyNote2.git"
     LOG_FILE = os.path.join(PROJECT_PATH, "log", "{}.log".format(datetime.now().strftime("%Y-%m-%d")))
     JSON_DB_FILE = os.path.join(NOTES_DIRCTORY, "ideanote.json")
     CREATE_DB_IF_NOT_FOUND = True
