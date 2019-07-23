17c5801e589404412a36bb4a0f8c5834d6edd18a
wangms <wangms@create80.com>
2019-07-23 22:48:05 +0800

    ideanote3.x开发计划

--- start.bat
+++ start.bat
@@ -1,31 +1,31 @@
 @echo off
 echo   /$$$$$$       /$$                     /$$   /$$             /$$
-echo  ^|_  $$_/      ^| $$                    ^| $$$ ^| $$            ^| $$              
-echo    ^| $$    /$$$$$$$  /$$$$$$   /$$$$$$ ^| $$$$^| $$  /$$$$$$  /$$$$$$    /$$$$$$ 
+echo  ^|_  $$_/      ^| $$                    ^| $$$ ^| $$            ^| $$
+echo    ^| $$    /$$$$$$$  /$$$$$$   /$$$$$$ ^| $$$$^| $$  /$$$$$$  /$$$$$$    /$$$$$$
 echo    ^| $$   /$$__  $$ /$$__  $$ ^|____  $$^| $$ $$ $$ /$$__  $$^|_  $$_/   /$$__  $$
 echo    ^| $$  ^| $$  ^| $$^| $$$$$$$$  /$$$$$$$^| $$  $$$$^| $$  \ $$  ^| $$    ^| $$$$$$$$
 echo    ^| $$  ^| $$  ^| $$^| $$_____/ /$$__  $$^| $$\  $$$^| $$  ^| $$  ^| $$ /$$^| $$_____/
 echo   /$$$$$$^|  $$$$$$$^|  $$$$$$$^|  $$$$$$$^| $$ \  $$^|  $$$$$$/  ^|  $$$$/^|  $$$$$$$
 echo  ^|______/ \_______/ \_______/ \_______/^|__/  \__/ \______/    \___/   \_______/
-echo ???????IdeaNote,?????...
+echo 准备启动IdeaNote,请稍后...
 echo=
-echo 1. ???IdeaNote
+echo 1. 关闭IdeaNote
 wmic process where "CommandLine like '%%IdeaNote%%' and name='python.exe'" call terminate
 echo=
-echo 2. ????IdeaNote
-git pull origin
+echo 2. 更新IdeaNote
+git pull
 
-rem ????????
+rem 定义笔记目录
 set NoteDir=E:\MyNote
 
-rem ?????????
+rem 定义服务端口
 set Port=5555
 
-rem ????IdeaNote???????????
-set NoteApp=E:\Source\Python\IdeaNote\app.py
+rem 定义IdeaNote的主程序文件
+set NoteApp=E:\DataCarrer\IdeaNote2.0\app.py
 
-echo 3. ????IdeaNote
+echo 3. 启动IdeaNote
 start /b python %NoteApp% %Port% %NoteDir%
 
-echo IdeaNote???????, ??????
+echo IdeaNote启动成功, 准备退出
 pause
