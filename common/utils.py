a19a9d73ebe1f2f693573c3e4f4025cfdca3eb63
wangms <wangms@create80.com>
2019-11-14 23:57:43 +0800

    fixed: 删除不必要的配置

--- common/utils.py
+++ common/utils.py
@@ -30,7 +30,7 @@ def timestamp_max(ts1, ts2):
 class ConfigLoader(object):
     def __init__(self):
         project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-        with open(os.path.join(project_root, "config.sample.yml"), "r", encoding="utf8") as f:
+        with open(os.path.join(project_root, "config.yml"), "r", encoding="utf8") as f:
             self.config = yaml.load(f.read(), Loader=yaml.FullLoader)
 
     @property
