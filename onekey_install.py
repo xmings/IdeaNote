ae70c04a3876a112679fdedafc41f3d5a31bd84f
wangms <wangms@create80.com>
2021-02-07 14:01:06 +0800

    fixed: 修复onekey_install.py缺陷

--- onekey_install.py
+++ onekey_install.py
@@ -6,6 +6,8 @@
 import logging
 import os
 import sys
+from colorama import init as cmd_color_init, Fore, Back
+
 
 welcome = """# 欢迎使用IdeaNote - 助你打造自己的知识平台
 ============
@@ -20,48 +22,46 @@ welcome = """# 欢迎使用IdeaNote - 助你打造自己的知识平台
 
 > 如在使用过程中有任何奇思妙想都可以在Github上发起issue，同时也期待你加入我们的开发团队。
 """
+logging.getLogger(name="IdeaNoteSync").disabled = True
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.INFO)
-logger.addHandler(console_handler)
+cmd_color_init(autoreset=True)
 
-v = input("\033[1;31m请注意，该脚本是IdeaNote的初始化安装脚本，一旦继续运行就会清除IdeaNote的笔记数据 \033[0m\n确认这不是误操作, 请输入Y：")
-if v != "Y":
+print(Fore.RED + "注意：该脚本是IdeaNote的初始化安装脚本，一旦继续会清除IdeaNote的笔记数据!!")
+v = input("是否需要继续（Y：继续，N：退出）：")
+if v.upper() != "Y":
     sys.exit(0)
-
-logger.info("==========初始化IdeaNote开始===========")
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
-logger.info("1. 安装依赖包")
+print("1. 安装依赖包")
+
 import platform
 requirements_file = os.path.join(current_dir, "requirements.txt")
 with open(requirements_file, "r", encoding="utf8") as f:
     requirements = [i.strip() for i in f.readlines()]
-if platform.system() == "Windows":
-    requirements.append("pypiwin32")
+if platform.system() != "Windows":
+    requirements.remove("pywin32")
 
 import subprocess
-completed = subprocess.run(f"pip install {' '.join(requirements)}", shell=True,
-                           stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding="utf8")
-logger.info(completed.stdout)
+for line in requirements:
+    print(f"安装{line}")
+    completed = subprocess.run(f"pip install {line}", shell=True,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE, encoding="utf8")
 if completed.returncode != 0:
-    logger.error(completed.stderr)
+    print(Fore.RED + completed.stderr)
     sys.exit(1)
-logger.info(">>>>>>完成！\n")
+print(">>>>>>完成！\n")
 
-
-logger.info("2. 检查配置文件")
+print("2. 检查配置文件")
 config_file = os.path.join(current_dir, "config.yml")
 if not os.path.exists(config_file):
-    logger.error(f"\033[1;31m {config_file}文件不存在， 请复制config.sample.yml为config.yml，并修改其中的必要配置信息 \033[0m")
+    print(Fore.RED + f"{config_file}文件不存在， 请复制config.sample.yml为config.yml，并修改其中的必要配置信息")
     sys.exit(1)
-logger.info(">>>>>>通过！\n")
+print(">>>>>>通过！\n")
 
 from common import conf
-logger.info("3. 初始化同步目录")
+print("3. 初始化同步目录")
 if conf.sync_method == "github":
     from sync.sync_utils.github_sync_utils import GithubSyncUtils
     GithubSyncUtils(conf.sync_connection_info).init_version_info()
@@ -69,22 +69,24 @@ elif conf.sync_method == "netdisk":
     from sync.sync_utils.netdisk_sync_utils import NetDiskSyncUtils
     NetDiskSyncUtils(conf.sync_work_dir).init_version_info()
 else:
-    logger.error("\033[1;31m 错误的同步方式配置，请正确填写config.yml中的sync_method的值\033[0m")
+    print(Fore.RED + "错误的同步方式配置，请正确填写config.yml中的sync_method的值")
     sys.exit(1)
 
-logger.info(">>>>>>通过！\n")
+print(">>>>>>通过！\n")
 
-logger.info("4. 检查日志目录")
+print("4. 检查日志目录")
 if not os.path.exists(conf.log_directory):
-    logger.error("\033[1;31m 请在config.yml文件中正确指定log_directory的路径 \033[0m")
+    print(Fore.RED + "请在config.yml文件中正确指定log_directory的路径")
     sys.exit(1)
-logger.info(">>>>>>通过！\n")
+print(">>>>>>通过！\n")
 
-logger.info("5. 初始化DB")
+print("5. 初始化DB")
+import zlib
+import sqlalchemy
+from sqlalchemy.orm import sessionmaker
 from core.model import Catalog, Image, SyncRecord
 from sync.model import SyncInfo
-import sqlalchemy, zlib
-from sqlalchemy.orm import sessionmaker
+
 
 db_file = conf.db_config.get("SQLALCHEMY_DATABASE_URI")
 db = sqlalchemy.create_engine(db_file)
@@ -92,9 +94,15 @@ session = sessionmaker(bind=db)()
 for t in [Catalog, Image, SyncRecord, SyncInfo]:
     t.metadata.drop_all(db.engine)
     t.metadata.create_all(db.engine)
-root = Catalog(title="MyNote", parent_id="self", content=zlib.compress(welcome.encode("utf8")), status=1)
-session.add(root)
+root_node = Catalog(title="MyNote", parent_id="self", content=zlib.compress(welcome.encode("utf8")), status=1)
+session.add(root_node)
+sync_init = SyncInfo(current_version=0, latest_version=0)
+session.add(sync_init)
 session.commit()
-logger.info(">>>>>>完成！\n")
+print(">>>>>>完成！\n")
+print("安装成功，手工执行以下命令启动IdeaNote:")
+print(Fore.GREEN + f"python {os.path.join(current_dir, 'app.py')} ")
 
-logger.info(f"\033[1;31mIdeaNote安装成功，但还需要在命令行执行以下命令以启动IdeaNote:\033[0m\npython {os.path.join(current_dir, 'app.py')} ")
+
+input("回车退出...\n")
+
